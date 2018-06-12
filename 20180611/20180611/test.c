dbb8afbdd24190412d3806413fdd57848da764f1
KKim sang heon <tkdgjs1501@nate.com>
2018-06-12 10:46:57 +0900

    0612 init

--- 20180611/20180611/test.c
+++ 20180611/20180611/test.c
@@ -1,4 +1,15 @@
 #include<stdio.h>
-int main(void) {
-	printf("");
+int Add(int a, int b);
+int main(void) 
+{
+	int res = 0;
+	res = Add(3, 5);
+	return 0;
+}
+int Add(int a, int b) 
+{
+	int sum = 0;
+	sum = a + b;
+	return sum;
+
 }

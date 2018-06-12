f728cb24752f349f2454c1de4b17ca7bdc7aac7a
KKim sang heon <tkdgjs1501@nate.com>
2018-06-12 10:52:26 +0900

    0612 init commit

---
+++ 20180612/20180612/inputTest.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+	int age = 0;
+	int year;
+
+	printf("홍길동");
+
+	printf("나이:");
+	scanf("%d", &age);
+
+	printf("번:");
+	scanf("%d", &year);
+
+	printf("%d\n", age);
+	printf("%d", year);
+
+	printf("age address : 0x%p\n", &age);
+	printf("year address : 0x%p\n", &year);
+	return 0;
+}

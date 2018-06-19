13c9fb34d861916d598a5b5eb843fc0ad0237c5e
KKim sang heon <tkdgjs1501@nate.com>
2018-06-19 09:35:48 +0900

    20180619 init

---
+++ 20180619/20180619/test.cpp
@@ -0,0 +1,43 @@
+
+#include <iostream>       // std::cout
+#include <string>         // std::string
+
+
+using namespace std;
+
+/*
+* Complete the timeConversion function below.
+*/
+string timeConversion(string s) {
+
+	string hour = s.substr(0, 2);
+	int iHour = atoi(hour.data());
+	size_t found = s.find("PM");
+	if (found != std::string::npos)
+	{
+		iHour += 12;
+		hour = itoa(iHour);
+		for (int i = 0; i<hour.length(); i++)
+		{
+			s[i] = hour[i];
+
+		}
+	}
+	return s;
+}
+
+int main()
+{
+	ofstream fout(getenv("OUTPUT_PATH"));
+
+	string s;
+	getline(cin, s);
+
+	string result = timeConversion(s);
+
+	fout << result << "\n";
+
+	fout.close();
+
+	return 0;
+}

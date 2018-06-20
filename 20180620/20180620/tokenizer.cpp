9ba05b9807ac4126b2fd7608f0681a440735b85e
KKim sang heon <tkdgjs1501@nate.com>
2018-06-20 17:35:53 +0900

    final commit

--- 20180620/20180620/tokenizer.cpp
+++ 20180620/20180620/tokenizer.cpp
@@ -1,7 +1,6 @@
-
 #include <iostream>
 #include <cstring>
-#include <cstdio>
+#include <sstream>
 
 using namespace std;
 
@@ -13,31 +12,97 @@ typedef struct person
 	struct person* next;
 }PER;
 
-void parsingCanvasSize(char * string);
+void parsingCanvasSize(char * str);
+void parsingBGColor(char * str);
+int parsingFrame(char * str);
+void parsingMeta(char * str);
 int main() {
 	char input[100];
-	cin.getline(input, 100);
-	printf("%s\n",input);
 	
+	cin.getline(input, 100);
 	parsingCanvasSize(input);
+	
+	cin.getline(input, 100);
+	parsingBGColor(input);
 
+	int frames;
+	cin.getline(input, 100);
+	frames = parsingFrame(input);
+
+	cin.getline(input, 100);
+
+	for (int i = 0; i < frames; i++)
+	{
+		cin.getline(input, 100);
+		parsingMeta(input);
+	}
 
 	return 0;
 }
 
 void parsingCanvasSize(char * str)
 {
-	char *name, *data, *width, *height;	
+	char *data, *width, *height;	
 	int iWidth, iHeight;
-	name = strtok(str, ":");
-	data = strtok(NULL, "");
+	data = strtok(str, ":");
+	data = strtok(NULL, ":");
 
 
 	width = strtok(data, "x");
 	height = strtok(NULL, "x");
 
+	cout << atoi(width)<<"\n";
+	cout << atoi(height)<<"\n";
+}
+void parsingBGColor(char * str)
+{
+	string line = str;
+	string data = line.substr(line.find("0x") + 2, line.length()-1);
 
-	printf("%d", atoi(width));
-	printf("%d", atoi(height));
-	//cout << data;
+	unsigned int iHex;
+	std::stringstream ss;
+	ss << std::hex << data.data();
+	ss >> iHex;
+	
+	cout << iHex << "\n";
+}
+
+int parsingFrame(char * str)
+{
+	int frames;
+	string line = str;
+	string data = line.substr(line.find(":") + 1, line.length() - 1);
+
+	frames = atoi(data.data());
+	cout << frames << "\n";
+
+	return frames;
+}
+
+void parsingMeta(char * str)
+{
+	string line = str;
+	string sData = line.substr(line.find(":") + 1, line.length() - 1);
+
+	char *width, *height, *alpha, *duration;
+	char cData[100];	
+	strcpy(cData, sData.data());
+
+	width = strtok(cData, " ");
+	height = strtok(NULL, " ");
+	alpha = strtok(NULL, " ");
+	duration = strtok(NULL, " ");
+	int alphaChecker;
+
+	if (strcmp(alpha, "yes") == 0) 
+	{
+		alphaChecker = 1;
+	}
+	else 
+	{
+		alphaChecker = 0;
+	}
+
+	cout << width<<"x"<<height<<", "<< duration<<", "<< alphaChecker<<"\n";
+
 }
